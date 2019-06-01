e72f8fdea77c1bc0940a3d02a26b30baaab3979e
GOKOP <igor.bugajski@wp.pl>
2019-06-01 22:06:39 +0200

    init

---
+++ README.md
@@ -0,0 +1,8 @@
+Bash script to replace text with emoji with letters, if possible.
+
+Usage:
+```
+./text-to-emoji.sh "The most boring sentence in the world."
+```
+
+Requires bash (obviously), bc, awk and coreutils (namely cat and head)
---
+++ text-to-emoji.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# substitute $1 characters with appropiate dic.list entry
+function subst() {
+	snip=${orig::$1}
+	match=$(cat dic.list | egrep "^$snip " | head -n 1 | awk '{print $2}')
+	[ -z $match ] && return 1
+
+	orig=${orig:$1}
+	emoji="$emoji$match"
+}
+
+# copy character if it can't be replaced
+function copy() {
+	letter=${orig::1}
+	if [[ $letter == " " ]]; then
+		letter="   "               # enlarge spaces so they stand out
+	else 
+		letter=$(echo $letter | awk '{print toupper($0)}')
+	fi
+	emoji="$emoji$letter"
+	orig=${orig:1}
+}
+
+orig=$(echo $1 | awk '{print tolower($0)}')
+while [ ! -z "$orig" ]; do
+	len=4
+	succ=1
+	while (( $len > 0 )) && [ $succ == 1 ]; do
+		subst $len
+		succ=$?
+		len=$(echo "$len-1" | bc)
+	done
+	[ $succ == 1 ] && copy
+done
+
+echo "$emoji"
